845251f7ff72d0063563f92d651e82514df442db
DarkAsirius <kirill.samokhin.05@mail.ru>
2023-12-17 22:25:52 +0300

    files

---
+++ static/js/main.js
@@ -0,0 +1,54 @@
+const a1 = document.getElementById("a1");
+const a2 = document.getElementById("a2");
+const a3 = document.getElementById("a3");
+const a4 = document.getElementById("a4");
+const a5 = document.getElementById("a5");
+const a6 = document.getElementById("a6");
+
+a1.addEventListener('mouseover', function handleMouseOver() {
+    a1.style.color = "blue";
+});
+
+a1.addEventListener('mouseout', function handleMouseOut() {
+    a1.style.color = "#455868";
+});
+
+a2.addEventListener('mouseover', function handleMouseOver() {
+    a2.style.color = "blue";
+});
+
+a2.addEventListener('mouseout', function handleMouseOut() {
+    a2.style.color = "#455868";
+});
+
+a3.addEventListener('mouseover', function handleMouseOver() {
+    a3.style.color = "blue";
+});
+
+a3.addEventListener('mouseout', function handleMouseOut() {
+    a3.style.color = "#455868";
+});
+
+a4.addEventListener('mouseover', function handleMouseOver() {
+    a4.style.color = "blue";
+});
+
+a4.addEventListener('mouseout', function handleMouseOut() {
+    a4.style.color = "#455868";
+});
+
+a5.addEventListener('mouseover', function handleMouseOver() {
+    a5.style.color = "blue";
+});
+
+a5.addEventListener('mouseout', function handleMouseOut() {
+    a5.style.color = "#455868";
+});
+
+a6.addEventListener('mouseover', function handleMouseOver() {
+    a6.style.color = "blue";
+});
+
+a6.addEventListener('mouseout', function handleMouseOut() {
+    a6.style.color = "#455868";
+});
